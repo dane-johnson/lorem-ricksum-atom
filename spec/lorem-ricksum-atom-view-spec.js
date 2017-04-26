48d3619f341f0930448ee83f40a4cdd38f6fd339
Dane Johnson <dane_johnson@ymail.com>
2017-04-26 15:52:29 -0500

    cleaned up extra crud

--- spec/lorem-ricksum-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LoremRicksumAtomView from '../lib/lorem-ricksum-atom-view';
-
-describe('LoremRicksumAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
